a5ad4bfc82e3c77a01802328371fe146526d91e8
jaychou <111561075+updateforever@users.noreply.github.com>
2023-09-01 15:56:32 +0800

    Update local.py

--- local.py
+++ local.py
@@ -1,20 +1,13 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = '/home/micros/projects/OSTrack-main/result'  # Base directory for saving network checkpoints.
+        self.workspace_dir = ''  # Base directory for saving network checkpoints.
         # self.workspace_dir + '/pretrained_networks/'
-        self.lasot_dir = '/mnt/first/hushiyu/SOT/LaSOT/data'
-        self.got10k_dir = '/mnt/first/hushiyu/SOT/GOT-10k/data'
-        self.BioDrone_dir = '/mnt/second/hushiyu/UAV/BioDrone'
-        self.trackingnet_dir = '/mnt/second/wangyipei/trackingnet'
-        self.coco_dir = '/mnt/second/wangyipei/coco_root'
-        self.otb_dir = '/mnt/first/hushiyu/SOT/OTB/data'
+        self.lasot_dir = '/mnt/first/xxx/SOT/LaSOT/data'
+        self.got10k_dir = '/mnt/first/xxx/SOT/GOT-10k/data'
+        self.trackingnet_dir = '/mnt/second/xxx/trackingnet'
+        self.coco_dir = '/mnt/second/xxx/coco_root'
+        self.otb_dir = '/mnt/first/xxx/SOT/OTB/data'
         self.vot_dir = ''
-        self.imagenet_dir = ''
-        self.imagenetdet_dir = ''
-
-        #
-        self.lasotSOI_dir = '/mnt/first/hushiyu/SOT/LaSOT/data'  # '/mnt/second/wangyipei/SOI/data/lasot/data'
-        self.got10k_dir = '/mnt/first/hushiyu/SOT/GOT-10k/data'  # '/mnt/second/wangyipei/SOI/GOT-10k/data'
 
     def find_root_dir(self, dataset_str=None):
         if dataset_str is None:
